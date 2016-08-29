74a4f8c14f427587747f1e041ba4ec8095bd654e
usausa <machi.pon@gmail.com>
2016-08-29 11:37:37 +0900

    Update ServiceProvider

---
+++ Smart.Navigation/Navigation/DefaultServiceProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace Smart.Navigation
+{
+    using System;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class DefaultServiceProvider : IServiceProvider
+    {
+        public object GetService(Type serviceType)
+        {
+            return Activator.CreateInstance(serviceType);
+        }
+    }
+}
