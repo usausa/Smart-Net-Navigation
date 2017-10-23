235241459420a3545b0bfab552200f0695cf705f
usausa <machi.pon@gmail.com>
2017-10-23 20:48:40 +0900

    Add interfaces

---
+++ Smart.Navigation/Navigation/INavigationParameter.cs
@@ -0,0 +1,55 @@
+﻿namespace Smart.Navigation
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public interface INavigationParameter
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        T GetValue<T>(string key);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        T GetValue<T>();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        T GetValueOrDefault<T>(string key);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        T GetValueOrDefault<T>();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        T GetValueOr<T>(string key, T defaultValue);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        T GetValueOr<T>(T defaultValue);
+    }
+}
