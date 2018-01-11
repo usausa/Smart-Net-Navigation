0f28d618592245d0493329c6c83e62aab882ae87
usausa <machi.pon@gmail.com>
2018-01-11 19:08:19 +0900

    Add interface

---
+++ Smart.Navigation/Navigation/INavigator.cs
@@ -0,0 +1,26 @@
+﻿namespace Smart.Navigation
+{
+    using Smart.Navigation.Strategies;
+
+    public interface INavigator
+    {
+        int StackedCount { get; }
+
+        object CurrentPageId { get; }
+
+        object CurrentPageDomain { get; }
+
+        object CurrentPage { get; }
+
+        object CurrentTarget { get; }
+
+        // Exit
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", Justification = "Ignore")]
+        void Exit();
+
+        // Navigate
+
+        void Navigate(INavigationStrategy strategy, INavigationParameter parameter);
+    }
+}
---
+++ Smart.Navigation/NavigatorExtensions.Notify.cs
@@ -0,0 +1,19 @@
+﻿namespace Smart.Navigation
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public static partial class NavigatorExtensions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="navigator"></param>
+        /// <param name="parameter"></param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Extensions")]
+        public static void Notify(this INavigator navigator, object parameter)
+        {
+            (navigator.CurrentTarget as INotifySupport)?.NavigatorNotify(parameter);
+        }
+    }
+}
