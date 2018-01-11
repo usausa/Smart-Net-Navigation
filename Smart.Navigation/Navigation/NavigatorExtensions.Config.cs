024254eac38c3c115e2f07e64d386cace9c85368
usausa <machi.pon@gmail.com>
2018-01-11 19:15:11 +0900

    Update extensions

---
+++ Smart.Navigation/Navigation/NavigatorExtensions.Config.cs
@@ -0,0 +1,101 @@
+﻿//namespace Smart.Navigation
+//{
+//    using System;
+
+//    using Smart.Navigation.Components;
+//    using Smart.Navigation.Plugins;
+
+//    /// <summary>
+//    ///
+//    /// </summary>
+//    public static partial class NavigatorExtensions
+//    {
+//        public static Navigator UseProvider(this Navigator navigator, INavigationProvider provider)
+//        {
+//            if (navigator == null)
+//            {
+//                throw new ArgumentNullException(nameof(navigator));
+//            }
+
+//            if (provider == null)
+//            {
+//                throw new ArgumentNullException(nameof(provider));
+//            }
+
+//            navigator.Configure(c => c.Register(provider));
+
+//            return navigator;
+//        }
+
+//        public static Navigator UseActivator(this Navigator navigator, IActivator activator)
+//        {
+//            if (navigator == null)
+//            {
+//                throw new ArgumentNullException(nameof(navigator));
+//            }
+
+//            if (activator == null)
+//            {
+//                throw new ArgumentNullException(nameof(activator));
+//            }
+
+//            navigator.Configure(c => c.Register(activator));
+
+//            return navigator;
+//        }
+
+//        public static Navigator UseActivator(this Navigator navigator, Func<Type, object> callback)
+//        {
+//            if (callback == null)
+//            {
+//                throw new ArgumentNullException(nameof(callback));
+//            }
+
+//            return navigator.UseActivator(new CallbackActivator(callback));
+//        }
+
+//        public static Navigator UseConverter(this Navigator navigator, IConverter converter)
+//        {
+//            if (navigator == null)
+//            {
+//                throw new ArgumentNullException(nameof(navigator));
+//            }
+
+//            if (converter == null)
+//            {
+//                throw new ArgumentNullException(nameof(converter));
+//            }
+
+//            navigator.Configure(c => c.Register(converter));
+
+//            return navigator;
+//        }
+
+//        public static Navigator UseConverter(this Navigator navigator, Func<object, Type, object> callback)
+//        {
+//            if (callback == null)
+//            {
+//                throw new ArgumentNullException(nameof(callback));
+//            }
+
+//            return navigator.UseConverter(new CallbackConverter(callback));
+//        }
+
+//        public static Navigator UsePlugin(this Navigator navigator, IPlugin plugin)
+//        {
+//            if (navigator == null)
+//            {
+//                throw new ArgumentNullException(nameof(navigator));
+//            }
+
+//            if (plugin == null)
+//            {
+//                throw new ArgumentNullException(nameof(plugin));
+//            }
+
+//            navigator.Configure(c => c.Get<IPluginPipeline>().Plugins.Add(plugin));
+
+//            return navigator;
+//        }
+//    }
+//}
---
+++ Smart.Navigation/Navigation/NavigatorExtensions.Navigation.cs
@@ -0,0 +1,70 @@
+﻿//namespace Smart.Navigation
+//{
+//    using System;
+
+//    using Smart.Navigation.Strategies;
+
+//    public static partial class NavigatorExtensions
+//    {
+//        public static void Forward2(this INavigator navigator, object id)
+//        {
+//            navigator.Navigate(new ForwardStrategy(id), null);
+//        }
+
+//        public static void Forward2(this INavigator navigator, object id, INavigationParameter parameter)
+//        {
+//            navigator.Navigate(new ForwardStrategy(id), parameter);
+//        }
+
+//        public static void Push2(this INavigator navigator, object id)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public static void Push2(this INavigator navigator, object id, INavigationParameter parameter)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public static void Pop2(this INavigator navigator)
+//        {
+//            // TODO level nullable?
+//            throw new NotImplementedException();
+//        }
+
+//        public static void Pop2(this INavigator navigator, INavigationParameter parameter)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public static void Pop2(this INavigator navigator, int level)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public static void Pop2(this INavigator navigator, int level, INavigationParameter parameter)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public static void PopAndForward2(this INavigator navigator, object id)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public static void PopAndForward2(this INavigator navigator, object id, int level)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public static void PopAndForward2(this INavigator navigator, object id, INavigationParameter parameter)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public static void PopAndForward2(this INavigator navigator, object id, int level, INavigationParameter parameter)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
