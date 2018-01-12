1cc80aa819ffe2f97c36286f2603f44c81ae9642
usausa <machi.pon@gmail.com>
2018-01-12 10:04:55 +0900

    Add navigation provider

---
+++ Smart.Navigation/Navigation/INavigationProvider.cs
@@ -0,0 +1,21 @@
+﻿namespace Smart.Navigation
+{
+    using System;
+
+    public interface INavigationProvider
+    {
+        bool IsAsync { get; }
+
+        void BeginInvoke(Action action);
+
+        object ResolveTarget(object page);
+
+        void OpenPage(object page);
+
+        void ClosePage(object page);
+
+        void ActivePage(object page, object parameter);
+
+        object DectivePage(object page);
+    }
+}
