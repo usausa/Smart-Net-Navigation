52f1c4f57c3ddbdb943eeb2c0761d6df6eff82b7
usausa <machi.pon@gmail.com>
2018-01-12 09:54:39 +0900

    Add scope attribute

---
+++ Smart.Navigation/Navigation/Plugins/Scope/ScopeAttribute.cs
@@ -0,0 +1,32 @@
+﻿namespace Smart.Navigation.Plugins.Scope
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class ScopeAttribute : Attribute
+    {
+        public string Key { get; }
+
+        public Type RequestType { get; }
+
+        public ScopeAttribute()
+        {
+        }
+
+        public ScopeAttribute(string key)
+            : this(key, null)
+        {
+        }
+
+        public ScopeAttribute(Type requestType)
+            : this(null, requestType)
+        {
+        }
+
+        public ScopeAttribute(string key, Type requestType)
+        {
+            Key = key;
+            RequestType = requestType;
+        }
+    }
+}
