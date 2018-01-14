1e8433cf01e1f56a3cecb17f59246d506034c3d0
usausa <machi.pon@gmail.com>
2018-01-14 18:19:23 +0900

    Update config extensions

--- Smart.Navigation/Navigation/NavigatorConfigExtensions.cs
+++ Smart.Navigation/Navigation/NavigatorConfigExtensions.cs
@@ -8,6 +8,23 @@
 
     public static class NavigatorConfigExtensions
     {
+        public static NavigatorConfig UseProvider<TProvider>(this NavigatorConfig config)
+            where TProvider : INavigationProvider
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            config.Configure(c =>
+            {
+                c.RemoveAll<INavigationProvider>();
+                c.Add<INavigationProvider, TProvider>();
+            });
+
+            return config;
+        }
+
         public static NavigatorConfig UseProvider(this NavigatorConfig config, INavigationProvider provider)
         {
             if (config == null)
@@ -24,6 +41,23 @@
             {
                 c.RemoveAll<INavigationProvider>();
                 c.Add(provider);
+            });
+
+            return config;
+        }
+
+        public static NavigatorConfig UseFactory<TFactory>(this NavigatorConfig config)
+            where TFactory : IFactory
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            config.Configure(c =>
+            {
+                c.RemoveAll<IFactory>();
+                c.Add<IFactory, TFactory>();
             });
 
             return config;
@@ -60,6 +94,23 @@
             return config.UseFactory(new CallbackFactory(callback));
         }
 
+        public static NavigatorConfig UseConverter<TConverter>(this NavigatorConfig config)
+            where TConverter : IConverter
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            config.Configure(c =>
+            {
+                c.RemoveAll<IConverter>();
+                c.Add<IConverter, TConverter>();
+            });
+
+            return config;
+        }
+
         public static NavigatorConfig UseConverter(this NavigatorConfig config, IConverter converter)
         {
             if (config == null)
@@ -91,6 +142,19 @@
             return config.UseConverter(new CallbackConverter(callback));
         }
 
+        public static NavigatorConfig AddPlugin<TPlugin>(this NavigatorConfig config)
+            where TPlugin : IPlugin
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            config.Configure(c => c.Add<IPlugin, TPlugin>());
+
+            return config;
+        }
+
         public static NavigatorConfig AddPlugin(this NavigatorConfig config, IPlugin plugin)
         {
             if (config == null)
@@ -104,6 +168,23 @@
             }
 
             config.Configure(c => c.Add(plugin));
+
+            return config;
+        }
+
+        public static NavigatorConfig UseActivatorFactory<TActivatorFactory>(this NavigatorConfig config)
+            where TActivatorFactory : IActivatorFactory
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            config.Configure(c =>
+            {
+                c.RemoveAll<IActivatorFactory>();
+                c.Add<IActivatorFactory, TActivatorFactory>();
+            });
 
             return config;
         }
@@ -124,6 +205,23 @@
             {
                 c.RemoveAll<IActivatorFactory>();
                 c.Add(activatorFactory);
+            });
+
+            return config;
+        }
+
+        public static NavigatorConfig UseAccessorFactory<TAccessorFactory>(this NavigatorConfig config)
+            where TAccessorFactory : IAccessorFactory
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            config.Configure(c =>
+            {
+                c.RemoveAll<IAccessorFactory>();
+                c.Add<IAccessorFactory, TAccessorFactory>();
             });
 
             return config;
