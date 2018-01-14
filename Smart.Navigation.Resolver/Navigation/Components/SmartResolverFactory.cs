72e20a932a2c8e2d4bda2304b91bd28508422e2e
usausa <machi.pon@gmail.com>
2018-01-14 13:14:19 +0900

    Add resolver factory

---
+++ Smart.Navigation.Resolver/Navigation/Components/SmartResolverFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace Smart.Navigation.Components
+{
+    using System;
+
+    using Smart.Resolver;
+
+    public sealed class SmartResolverFactory : IFactory
+    {
+        private readonly IResolver resolver;
+
+        public SmartResolverFactory(IResolver resolver)
+        {
+            this.resolver = resolver;
+        }
+
+        public object Create(Type type)
+        {
+            return resolver.Get(type);
+        }
+    }
+}
---
+++ Smart.Navigation.Resolver/Navigation/ResolverNavigatorConfigExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Smart.Navigation
+{
+    using Smart.Navigation.Components;
+    using Smart.Resolver;
+
+    public static class ResolverNavigatorConfigExtensions
+    {
+        public static NavigatorConfig UseResolver(this NavigatorConfig config, IResolver resolver)
+        {
+            return config.UseFactory(new SmartResolverFactory(resolver));
+        }
+    }
+}
