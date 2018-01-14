322a604b17b90e5f3c0a24d4867f2730bf4eb40b
usausa <machi.pon@gmail.com>
2018-01-14 15:19:58 +0900

    Add test mock

---
+++ Smart.Navigation.Tests/Mock/MockPage.cs
@@ -0,0 +1,51 @@
+﻿namespace Smart.Mock
+{
+    using System;
+
+    public class MockPage : IDisposable
+    {
+        public string Name { get; }
+
+        public bool IsDisposed { get; private set; }
+
+        public bool IsOpen { get; private set; }
+
+        public bool IsVisible { get; set; }
+
+        public object Focused { get; set; }
+
+        public MockPage(string name)
+        {
+            Name = name;
+        }
+
+        ~MockPage()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && !IsDisposed)
+            {
+                IsDisposed = true;
+
+                IsOpen = false;
+                IsVisible = false;
+            }
+        }
+
+        public void Show()
+        {
+            IsOpen = true;
+            IsVisible = true;
+            Focused = this;
+        }
+    }
+}
---
+++ Smart.Navigation.Tests/Mock/MockPageNavigationProvider.cs
@@ -0,0 +1,45 @@
+﻿namespace Smart.Mock
+{
+    using Smart.Navigation;
+
+    public sealed class MockPageNavigationProvider : INavigationProvider
+    {
+        public object ResolveTarget(object page)
+        {
+            return page;
+        }
+
+        public void OpenPage(object page)
+        {
+            var mock = (MockPage)page;
+
+            mock.Show();
+        }
+
+        public void ClosePage(object page)
+        {
+            var mock = (MockPage)page;
+
+            mock.Dispose();
+        }
+
+        public void ActivePage(object page, object parameter)
+        {
+            var mock = (MockPage)page;
+
+            mock.IsVisible = true;
+            mock.Focused = parameter;
+        }
+
+        public object DectivePage(object page)
+        {
+            var mock = (MockPage)page;
+
+            mock.IsVisible = false;
+            var parameter = mock.Focused;
+            mock.Focused = null;
+
+            return parameter;
+        }
+    }
+}
