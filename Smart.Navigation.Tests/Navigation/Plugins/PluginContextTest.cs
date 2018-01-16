0fd6e3e47dea162cd8d8ecd24d1dc267644dfd09
usausa <machi.pon@gmail.com>
2018-01-16 14:28:29 +0900

    Add plugin context test

---
+++ Smart.Navigation.Tests/Navigation/Plugins/PluginContextTest.cs
@@ -0,0 +1,79 @@
+﻿namespace Smart.Navigation.Plugins
+{
+    using System;
+
+    using Xunit;
+
+    public static class PluginContextTest
+    {
+        [Fact]
+        public static void TestPluginContextLoadStoredValue()
+        {
+            var contect = new PluginContext();
+
+            contect.Save(typeof(string), "abc");
+            Assert.Equal("abc", contect.Load<string>(typeof(string)));
+        }
+
+        [Fact]
+        public static void TestPluginContextLoadOrDefault()
+        {
+            var contect = new PluginContext();
+
+            contect.Save(typeof(string), "abc");
+            Assert.Equal("abc", contect.LoadOr(typeof(string), string.Empty));
+        }
+
+        [Fact]
+        public static void TestPluginContextLoadOrDefaultNotPrepared()
+        {
+            var contect = new PluginContext();
+
+            Assert.Equal("abc", contect.LoadOr(typeof(string), "abc"));
+        }
+
+        [Fact]
+        public static void TestPluginContextLoadOrDefaultNotContained()
+        {
+            var contect = new PluginContext();
+
+            contect.Save(typeof(int), 123);
+            Assert.Equal("abc", contect.LoadOr(typeof(string), "abc"));
+        }
+
+        [Fact]
+        public static void TestPluginContextLoadOrDefaultByFactory()
+        {
+            var contect = new PluginContext();
+
+            contect.Save(typeof(string), "abc");
+            Assert.Equal("abc", contect.LoadOr(typeof(string), () => string.Empty));
+        }
+
+        [Fact]
+        public static void TestPluginContextLoadOrDefaultNotPreparedByFactory()
+        {
+            var contect = new PluginContext();
+
+            Assert.Equal("abc", contect.LoadOr(typeof(string), () => "abc"));
+        }
+
+        [Fact]
+        public static void TestPluginContextLoadOrDefaultNotContainedByFactory()
+        {
+            var contect = new PluginContext();
+
+            contect.Save(typeof(int), 123);
+            Assert.Equal("abc", contect.LoadOr(typeof(string), () => "abc"));
+        }
+
+        [Fact]
+        public static void TestPluginContextLoadFaiied()
+        {
+            var contect = new PluginContext();
+
+            contect.Save(typeof(int), 123);
+            Assert.Equal("abc", contect.LoadOr(typeof(string), (Func<string>)null));
+        }
+    }
+}
