dee2003f1dba13f8736d8701e6cb2f48e9d69a05
usausa <machi.pon@gmail.com>
2018-01-16 15:05:17 +0900

    Add notify test

---
+++ Smart.Navigation.Tests/Navigation/NavigatorNotifyTest.cs
@@ -0,0 +1,42 @@
+﻿namespace Smart.Navigation
+{
+    using Smart.Mock;
+
+    using Xunit;
+
+    public class NavigatorNotifyTest
+    {
+        [Fact]
+        public static void TestNavigatorNotify()
+        {
+            // prepare
+            var navigator = new NavigatorConfig()
+                .UseMockProvider()
+                .ToNavigator();
+
+            navigator.Register(Pages.NotifyPage, typeof(NotifyPage));
+
+            // test
+            navigator.Forward(Pages.NotifyPage);
+            navigator.Notify("test");
+
+            var notifyPage = (NotifyPage)navigator.CurrentPage;
+            Assert.Equal("test", notifyPage.Parameter);
+        }
+
+        public enum Pages
+        {
+            NotifyPage
+        }
+
+        public class NotifyPage : MockPage, INotifySupport
+        {
+            public object Parameter { get; private set; }
+
+            public void NavigatorNotify(object parameter)
+            {
+                Parameter = parameter;
+            }
+        }
+    }
+}
