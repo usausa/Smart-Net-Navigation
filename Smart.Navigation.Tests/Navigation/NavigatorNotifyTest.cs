cf3a4e3c1597bb17d9b1b111d34d5af0d74aeeb8
usausa <machi.pon@gmail.com>
2018-01-16 15:51:58 +0900

    Add vm notify test

--- Smart.Navigation.Tests/Navigation/NavigatorNotifyTest.cs
+++ Smart.Navigation.Tests/Navigation/NavigatorNotifyTest.cs
@@ -1,13 +1,18 @@
 ﻿namespace Smart.Navigation
 {
     using Smart.Mock;
+    using Smart.Resolver;
 
     using Xunit;
 
     public class NavigatorNotifyTest
     {
+        // ------------------------------------------------------------
+        // Page
+        // ------------------------------------------------------------
+
         [Fact]
-        public static void TestNavigatorNotify()
+        public static void TestPageNavigatorNotify()
         {
             // prepare
             var navigator = new NavigatorConfig()
@@ -36,6 +41,56 @@
             public void NavigatorNotify(object parameter)
             {
                 Parameter = parameter;
+            }
+        }
+
+        // ------------------------------------------------------------
+        // View
+        // ------------------------------------------------------------
+
+        [Fact]
+        public static void TestViewNavigatorNotify()
+        {
+            // prepare
+            var resolver = new ResolverConfig()
+                .UseAutoBinding()
+                .ToResolver();
+            var navigator = new NavigatorConfig()
+                .UseMockViewProvider()
+                .UseResolver(resolver)
+                .ToNavigator();
+
+            navigator.Register(Views.NotifyView, typeof(NotifyView));
+
+            // test
+            navigator.Forward(Views.NotifyView);
+            navigator.Notify("test");
+
+            var notifyView = (NotifyView)navigator.CurrentPage;
+            var notifyViewModel = (NotifyViewModel)notifyView.Context;
+            Assert.Equal("test", notifyViewModel.Parameter);
+        }
+
+        public enum Views
+        {
+            NotifyView
+        }
+
+        public class NotifyViewModel : INotifySupport
+        {
+            public object Parameter { get; private set; }
+
+            public void NavigatorNotify(object parameter)
+            {
+                Parameter = parameter;
+            }
+        }
+
+        public class NotifyView : MockView
+        {
+            public NotifyView(NotifyViewModel vm)
+            {
+                Context = vm;
             }
         }
     }
