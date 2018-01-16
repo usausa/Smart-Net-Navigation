fc0f27b37b8ab0a633342feca841e8d30ae19be5
usausa <machi.pon@gmail.com>
2018-01-16 15:10:22 +0900

    Add aware test

---
+++ Smart.Navigation.Tests/Navigation/NavigatorAwareTest.cs
@@ -0,0 +1,36 @@
+﻿namespace Smart.Navigation
+{
+    using Smart.Mock;
+
+    using Xunit;
+
+    public class NavigatorAwareTest
+    {
+        [Fact]
+        public static void TestNavigatorAware()
+        {
+            // prepare
+            var navigator = new NavigatorConfig()
+                .UseMockProvider()
+                .ToNavigator();
+
+            navigator.Register(Pages.AwarePage, typeof(AwarePage));
+
+            // test
+            navigator.Forward(Pages.AwarePage);
+
+            var awarePage = (AwarePage)navigator.CurrentPage;
+            Assert.Same(navigator, awarePage.Navigator);
+        }
+
+        public enum Pages
+        {
+            AwarePage
+        }
+
+        public class AwarePage : MockPage, INavigatorAware
+        {
+            public INavigator Navigator { get; set; }
+        }
+    }
+}
