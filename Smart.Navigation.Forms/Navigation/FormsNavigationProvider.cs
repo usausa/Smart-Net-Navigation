3fc7b34c051c4140de3f59decf6263001b50f588
usausa <machi.pon@gmail.com>
2018-01-28 16:55:57 +0900

    Update forms provider

--- Smart.Navigation.Forms/Navigation/FormsNavigationProvider.cs
+++ Smart.Navigation.Forms/Navigation/FormsNavigationProvider.cs
@@ -1,6 +1,33 @@
 ﻿namespace Smart.Navigation
 {
-    public class FormsNavigationProvider
+    using Xamarin.Forms;
+
+    public class FormsNavigationProvider : INavigationProvider
     {
+        public object ResolveTarget(object view)
+        {
+            var page = (ContentView)view;
+            return page.BindingContext;
+        }
+
+        public void OpenView(object view)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CloseView(object view)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ActiveView(object view, object parameter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object DeactiveView(object view)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
