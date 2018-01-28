0e751af9fa1e6416bd908b7e9cd9352c3f4453af
usausa <machi.pon@gmail.com>
2018-01-28 16:47:27 +0900

    Update windows navigation provider

--- Smart.Navigation.Windows/Navigation/WindowsNavigationProvider.cs
+++ Smart.Navigation.Windows/Navigation/WindowsNavigationProvider.cs
@@ -1,6 +1,33 @@
 ﻿namespace Smart.Navigation
 {
-    public class WindowsNavigationProvider
+    using System.Windows.Controls;
+
+    public class WindowsNavigationProvider : INavigationProvider
     {
+        public object ResolveTarget(object view)
+        {
+            var control = (Control)view;
+            return control.DataContext;
+        }
+
+        public void OpenView(object view)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CloseView(object view)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ActiveView(object view, object parameter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object DeactiveView(object view)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
