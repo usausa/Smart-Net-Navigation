7de6fe597e95fb8c3048a3980d1bcc462df2d0c0
usausa <machi.pon@gmail.com>
2018-01-31 11:14:53 +0900

    Update type register method

--- Smart.Navigation/Navigation/NavigatorExtensions.Register.cs
+++ Smart.Navigation/Navigation/NavigatorExtensions.Register.cs
@@ -20,9 +20,20 @@
             {
                 foreach (var attr in type.GetTypeInfo().GetCustomAttributes<ViewAttribute>())
                 {
-                    navigator.Register(attr.Id, type);
+                    navigator.Register(attr.Id ?? type, type);
                 }
             }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Extensions")]
+        public static void Register(this Navigator navigator, Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            navigator.Register(type, type);
         }
     }
 }
