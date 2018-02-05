4db3ca7566944ffe8aad52dd839fa22f5b2d856a
usausa <machi.pon@gmail.com>
2018-02-05 16:30:00 +0900

    Rename view model

--- Example.WindowsApp/Views/MenuViewModel.cs
+++ Example.WindowsApp/Views/MenuViewModel.cs
@@ -4,11 +4,11 @@
     using Smart.Navigation;
     using Smart.Windows.Input;
 
-    public class MenuViewViewModel : AppViewModelBase
+    public class MenuViewModel : AppViewModelBase
     {
         public AsyncCommand<Type> Forward { get; }
 
-        public MenuViewViewModel(INavigator navigator)
+        public MenuViewModel(INavigator navigator)
         {
             Forward = MakeAsyncCommand<Type>(navigator.ForwardAsync);
         }
