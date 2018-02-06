c0e216f6def6252c3dd82a77b1f747ca98971cfa
usausa <machi.pon@gmail.com>
2018-02-06 11:55:20 +0900

    Fix plugin test

--- Smart.Navigation.Tests/Navigation/Plugins/Parameter/ParameterPluginTest.cs
+++ Smart.Navigation.Tests/Navigation/Plugins/Parameter/ParameterPluginTest.cs
@@ -1,191 +1,168 @@
-﻿//namespace Smart.Navigation.Plugins.Parameter
-//{
-//    using Smart.Mock;
+﻿namespace Smart.Navigation.Plugins.Parameter
+{
+    using Smart.Mock;
 
-//    using Xunit;
+    using Xunit;
 
-//    public class ParameterPluginTest
-//    {
-//        [Fact]
-//        public static void TestParameter()
-//        {
-//            // prepare
-//            var navigator = new NavigatorConfig()
-//                .UseMockFormProvider()
-//                .ToNavigator();
+    public class ParameterPluginTest
+    {
+        [Fact]
+        public static void TestParameter()
+        {
+            // prepare
+            var navigator = new NavigatorConfig()
+                .UseMockFormProvider()
+                .ToNavigator();
 
-//            navigator.Register(ViewId.Form1, typeof(Form1));
-//            navigator.Register(ViewId.Form2, typeof(Form2));
+            // test
+            navigator.Forward(typeof(Form1));
 
-//            // test
-//            navigator.Forward(ViewId.Form1);
+            var form1 = (Form1)navigator.CurrentView;
+            form1.IntParameter = 123;
+            form1.StringParameter = "abc";
 
-//            var form1 = (Form1)navigator.CurrentView;
-//            form1.IntParameter = 123;
-//            form1.StringParameter = "abc";
+            navigator.Forward(typeof(Form2));
 
-//            navigator.Forward(ViewId.Form2);
+            Assert.Equal(1, navigator.StackedCount);
+            var form2 = (Form2)navigator.CurrentView;
+            Assert.Equal(123, form2.IntParameter);
+            Assert.Equal("abc", form2.StringParameter);
+        }
 
-//            Assert.Equal(1, navigator.StackedCount);
-//            var form2 = (Form2)navigator.CurrentView;
-//            Assert.Equal(123, form2.IntParameter);
-//            Assert.Equal("abc", form2.StringParameter);
-//        }
+        [Fact]
+        public static void TestParameterOneWay()
+        {
+            // prepare
+            var navigator = new NavigatorConfig()
+                .UseMockFormProvider()
+                .ToNavigator();
 
-//        [Fact]
-//        public static void TestParameterOneWay()
-//        {
-//            // prepare
-//            var navigator = new NavigatorConfig()
-//                .UseMockFormProvider()
-//                .ToNavigator();
+            // test
+            navigator.Forward(typeof(Form1));
 
-//            navigator.Register(ViewId.Form1, typeof(Form1));
-//            navigator.Register(ViewId.OneWay, typeof(OneWayForm));
+            var form1 = (Form1)navigator.CurrentView;
+            form1.IntParameter = 123;
+            form1.StringParameter = "abc";
 
-//            // test
-//            navigator.Forward(ViewId.Form1);
+            navigator.Push(typeof(OneWayForm));
 
-//            var form1 = (Form1)navigator.CurrentView;
-//            form1.IntParameter = 123;
-//            form1.StringParameter = "abc";
+            var oneWayForm = (OneWayForm)navigator.CurrentView;
+            Assert.Equal(123, oneWayForm.IntParameter);
+            Assert.Null(oneWayForm.StringParameter);
 
-//            navigator.Push(ViewId.OneWay);
+            oneWayForm.IntParameter = 987;
+            oneWayForm.StringParameter = "xyz";
 
-//            var oneWayForm = (OneWayForm)navigator.CurrentView;
-//            Assert.Equal(123, oneWayForm.IntParameter);
-//            Assert.Null(oneWayForm.StringParameter);
+            navigator.Pop();
+            Assert.Equal(123, form1.IntParameter);
+            Assert.Equal("xyz", form1.StringParameter);
+        }
 
-//            oneWayForm.IntParameter = 987;
-//            oneWayForm.StringParameter = "xyz";
+        [Fact]
+        public static void TestParameterNamed()
+        {
+            // prepare
+            var navigator = new NavigatorConfig()
+                .UseMockFormProvider()
+                .ToNavigator();
 
-//            navigator.Pop();
-//            Assert.Equal(123, form1.IntParameter);
-//            Assert.Equal("xyz", form1.StringParameter);
-//        }
+            // test
+            navigator.Forward(typeof(Named1Form));
 
-//        [Fact]
-//        public static void TestParameterNamed()
-//        {
-//            // prepare
-//            var navigator = new NavigatorConfig()
-//                .UseMockFormProvider()
-//                .ToNavigator();
+            var namedForm1 = (Named1Form)navigator.CurrentView;
+            namedForm1.Value1 = 123;
+            namedForm1.Parameter2 = "abc";
 
-//            navigator.Register(ViewId.Named1Form, typeof(Named1Form));
-//            navigator.Register(ViewId.Named2Form, typeof(Named2Form));
+            navigator.Forward(typeof(Named2Form));
 
-//            // test
-//            navigator.Forward(ViewId.Named1Form);
+            var namedForm2 = (Named2Form)navigator.CurrentView;
+            Assert.Equal(123, namedForm2.Parameter1);
+            Assert.Equal("abc", namedForm2.Value2);
+        }
 
-//            var namedForm1 = (Named1Form)navigator.CurrentView;
-//            namedForm1.Value1 = 123;
-//            namedForm1.Parameter2 = "abc";
+        [Fact]
+        public static void TestParameterConvert()
+        {
+            // prepare
+            var navigator = new NavigatorConfig()
+                .UseMockFormProvider()
+                .ToNavigator();
 
-//            navigator.Forward(ViewId.Named2Form);
+            // test
+            navigator.Forward(typeof(Convert1Form));
 
-//            var namedForm2 = (Named2Form)navigator.CurrentView;
-//            Assert.Equal(123, namedForm2.Parameter1);
-//            Assert.Equal("abc", namedForm2.Value2);
-//        }
+            var convertForm1 = (Convert1Form)navigator.CurrentView;
+            convertForm1.Value1 = 123;
+            convertForm1.Value2 = "456";
 
-//        [Fact]
-//        public static void TestParameterConvert()
-//        {
-//            // prepare
-//            var navigator = new NavigatorConfig()
-//                .UseMockFormProvider()
-//                .ToNavigator();
+            navigator.Forward(typeof(Convert2Form));
 
-//            navigator.Register(ViewId.Convert1Form, typeof(Convert1Form));
-//            navigator.Register(ViewId.Convert2Form, typeof(Convert2Form));
+            var convertForm2 = (Convert2Form)navigator.CurrentView;
+            Assert.Equal("123", convertForm2.Value1);
+            Assert.Equal(456, convertForm2.Value2);
+        }
 
-//            // test
-//            navigator.Forward(ViewId.Convert1Form);
+        public class Form1 : MockForm
+        {
+            [Parameter]
+            public int IntParameter { get; set; }
 
-//            var convertForm1 = (Convert1Form)navigator.CurrentView;
-//            convertForm1.Value1 = 123;
-//            convertForm1.Value2 = "456";
+            [Parameter]
+            public string StringParameter { get; set; }
+        }
 
-//            navigator.Forward(ViewId.Convert2Form);
+        public class Form2 : MockForm
+        {
+            [Parameter]
+            public int IntParameter { get; set; }
 
-//            var convertForm2 = (Convert2Form)navigator.CurrentView;
-//            Assert.Equal("123", convertForm2.Value1);
-//            Assert.Equal(456, convertForm2.Value2);
-//        }
+            [Parameter]
+            public string StringParameter { get; set; }
+        }
 
-//        public enum ViewId
-//        {
-//            Form1,
-//            Form2,
-//            OneWay,
-//            Named1Form,
-//            Named2Form,
-//            Convert1Form,
-//            Convert2Form
-//        }
+        public class OneWayForm : MockForm
+        {
+            [Parameter(Directions.Import)]
+            public int IntParameter { get; set; }
 
-//        public class Form1 : MockForm
-//        {
-//            [Parameter]
-//            public int IntParameter { get; set; }
+            [Parameter(Directions.Export)]
+            public string StringParameter { get; set; }
+        }
 
-//            [Parameter]
-//            public string StringParameter { get; set; }
-//        }
+        public class Named1Form : MockForm
+        {
+            [Parameter("Parameter1")]
+            public int Value1 { get; set; }
 
-//        public class Form2 : MockForm
-//        {
-//            [Parameter]
-//            public int IntParameter { get; set; }
+            [Parameter]
+            public string Parameter2 { get; set; }
+        }
 
-//            [Parameter]
-//            public string StringParameter { get; set; }
-//        }
+        public class Named2Form : MockForm
+        {
+            [Parameter]
+            public int Parameter1 { get; set; }
 
-//        public class OneWayForm : MockForm
-//        {
-//            [Parameter(Directions.Import)]
-//            public int IntParameter { get; set; }
+            [Parameter("Parameter2")]
+            public string Value2 { get; set; }
+        }
 
-//            [Parameter(Directions.Export)]
-//            public string StringParameter { get; set; }
-//        }
+        public class Convert1Form : MockForm
+        {
+            [Parameter]
+            public int Value1 { get; set; }
 
-//        public class Named1Form : MockForm
-//        {
-//            [Parameter("Parameter1")]
-//            public int Value1 { get; set; }
+            [Parameter]
+            public string Value2 { get; set; }
+        }
 
-//            [Parameter]
-//            public string Parameter2 { get; set; }
-//        }
+        public class Convert2Form : MockForm
+        {
+            [Parameter]
+            public string Value1 { get; set; }
 
-//        public class Named2Form : MockForm
-//        {
-//            [Parameter]
-//            public int Parameter1 { get; set; }
-
-//            [Parameter("Parameter2")]
-//            public string Value2 { get; set; }
-//        }
-
-//        public class Convert1Form : MockForm
-//        {
-//            [Parameter]
-//            public int Value1 { get; set; }
-
-//            [Parameter]
-//            public string Value2 { get; set; }
-//        }
-
-//        public class Convert2Form : MockForm
-//        {
-//            [Parameter]
-//            public string Value1 { get; set; }
-
-//            [Parameter]
-//            public int Value2 { get; set; }
-//        }
-//    }
-//}
+            [Parameter]
+            public int Value2 { get; set; }
+        }
+    }
+}
--- Smart.Navigation.Tests/Navigation/Plugins/Scope/ScopePluginTest.cs
+++ Smart.Navigation.Tests/Navigation/Plugins/Scope/ScopePluginTest.cs
@@ -1,248 +1,216 @@
-﻿//namespace Smart.Navigation.Plugins.Scope
-//{
-//    using System;
+﻿namespace Smart.Navigation.Plugins.Scope
+{
+    using System;
 
-//    using Smart.Mock;
+    using Smart.Mock;
 
-//    using Xunit;
+    using Xunit;
 
-//    public class ScopePluginTest
-//    {
-//        [Fact]
-//        public static void TestScope()
-//        {
-//            prepare
-//           var navigator = new NavigatorConfig()
-//               .UseMockFormProvider()
-//               .ToNavigator();
+    public class ScopePluginTest
+    {
+        [Fact]
+        public static void TestScope()
+        {
+            // prepare
+            var navigator = new NavigatorConfig()
+                .UseMockFormProvider()
+                .ToNavigator();
 
-//            navigator.Register(ViewId.Data1Form, typeof(Data1Form));
-//            navigator.Register(ViewId.Data2Form, typeof(Data2Form));
-//            navigator.Register(ViewId.Data3Form, typeof(Data3Form));
+            // test
+            navigator.Forward(typeof(Data1Form));
 
-//            test
-//            navigator.Forward(ViewId.Data1Form);
+            navigator.Forward(typeof(Data2Form));
 
-//            navigator.Forward(ViewId.Data2Form);
+            var form2 = (Data2Form)navigator.CurrentView;
+            Assert.NotNull(form2.Data);
+            Assert.True(form2.Data.IsInitialized);
+            Assert.False(form2.Data.IsDisposed);
 
-//            var form2 = (Data2Form)navigator.CurrentView;
-//            Assert.NotNull(form2.Data);
-//            Assert.True(form2.Data.IsInitialized);
-//            Assert.False(form2.Data.IsDisposed);
+            navigator.Forward(typeof(Data3Form));
 
-//            navigator.Forward(ViewId.Data3Form);
+            var form3 = (Data3Form)navigator.CurrentView;
+            Assert.Same(form3.Data, form2.Data);
+            Assert.True(form3.Data.IsInitialized);
+            Assert.False(form3.Data.IsDisposed);
 
-//            var form3 = (Data3Form)navigator.CurrentView;
-//            Assert.Same(form3.Data, form2.Data);
-//            Assert.True(form3.Data.IsInitialized);
-//            Assert.False(form3.Data.IsDisposed);
+            navigator.Forward(typeof(Data1Form));
 
-//            navigator.Forward(ViewId.Data1Form);
+            Assert.True(form3.Data.IsDisposed);
+        }
 
-//            Assert.True(form3.Data.IsDisposed);
-//        }
+        [Fact]
+        public static void TestScopeByRequestType()
+        {
+            // prepare
+            var navigator = new NavigatorConfig()
+                .UseMockFormProvider()
+                .ToNavigator();
 
-//        [Fact]
-//        public static void TestScopeByRequestType()
-//        {
-//            prepare
-//           var navigator = new NavigatorConfig()
-//               .UseMockFormProvider()
-//               .ToNavigator();
+            // test
+            navigator.Forward(typeof(Object1Form));
 
-//            navigator.Register(ViewId.Object1Form, typeof(Object1Form));
-//            navigator.Register(ViewId.Object2Form, typeof(Object2Form));
-//            navigator.Register(ViewId.Object3Form, typeof(Object3Form));
+            var form1 = (Object1Form)navigator.CurrentView;
+            Assert.NotNull(form1.Object);
 
-//            test
-//            navigator.Forward(ViewId.Object1Form);
+            navigator.Forward(typeof(Object2Form));
 
-//            var form1 = (Object1Form)navigator.CurrentView;
-//            Assert.NotNull(form1.Object);
+            var form2 = (Object2Form)navigator.CurrentView;
+            Assert.Same(form2.Object, form1.Object);
 
-//            navigator.Forward(ViewId.Object2Form);
+            navigator.Forward(typeof(Object3Form));
+        }
 
-//            var form2 = (Object2Form)navigator.CurrentView;
-//            Assert.Same(form2.Object, form1.Object);
+        [Fact]
+        public static void TestScopeSkipInTheMiddle()
+        {
+            // prepare
+            var navigator = new NavigatorConfig()
+                .UseMockFormProvider()
+                .ToNavigator();
 
-//            navigator.Forward(ViewId.Object3Form);
-//        }
+            // test
+            navigator.Forward(typeof(Push1Form));
 
-//        [Fact]
-//        public static void TestScopeSkipInTheMiddle()
-//        {
-//            prepare
-//           var navigator = new NavigatorConfig()
-//               .UseMockFormProvider()
-//               .ToNavigator();
+            navigator.Push(typeof(Push2Form));
 
-//            navigator.Register(ViewId.Push1Form, typeof(Push1Form));
-//            navigator.Register(ViewId.Push2Form, typeof(Push2Form));
-//            navigator.Register(ViewId.Push3Form, typeof(Push3Form));
-//            navigator.Register(ViewId.Push4Form, typeof(Push4Form));
+            var form2 = (Push2Form)navigator.CurrentView;
+            Assert.NotNull(form2.Data);
+            Assert.True(form2.Data.IsInitialized);
+            Assert.False(form2.Data.IsDisposed);
 
-//            test
-//            navigator.Forward(ViewId.Push1Form);
+            navigator.Push(typeof(Push3Form));
 
-//            navigator.Push(ViewId.Push2Form);
+            Assert.False(form2.Data.IsDisposed);
 
-//            var form2 = (Push2Form)navigator.CurrentView;
-//            Assert.NotNull(form2.Data);
-//            Assert.True(form2.Data.IsInitialized);
-//            Assert.False(form2.Data.IsDisposed);
+            navigator.Push(typeof(Push4Form));
 
-//            navigator.Push(ViewId.Push3Form);
+            var form4 = (Push4Form)navigator.CurrentView;
+            Assert.Same(form4.Data, form2.Data);
+            Assert.True(form4.Data.IsInitialized);
+            Assert.False(form4.Data.IsDisposed);
 
-//            Assert.False(form2.Data.IsDisposed);
+            navigator.Pop();
 
-//            navigator.Push(ViewId.Push4Form);
+            Assert.False(form4.Data.IsDisposed);
 
-//            var form4 = (Push4Form)navigator.CurrentView;
-//            Assert.Same(form4.Data, form2.Data);
-//            Assert.True(form4.Data.IsInitialized);
-//            Assert.False(form4.Data.IsDisposed);
+            navigator.Pop();
 
-//            navigator.Pop();
+            Assert.False(form4.Data.IsDisposed);
 
-//            Assert.False(form4.Data.IsDisposed);
+            navigator.Pop();
 
-//            navigator.Pop();
+            Assert.True(form4.Data.IsDisposed);
+        }
 
-//            Assert.False(form4.Data.IsDisposed);
+        [Fact]
+        public static void TestScopeNamed()
+        {
+            // prepare
+            var navigator = new NavigatorConfig()
+                .UseMockFormProvider()
+                .ToNavigator();
 
-//            navigator.Pop();
+            // test
+            navigator.Forward(typeof(Named1Form));
 
-//            Assert.True(form4.Data.IsDisposed);
-//        }
+            var form1 = (Named1Form)navigator.CurrentView;
+            Assert.NotNull(form1.ExportData);
 
-//        [Fact]
-//        public static void TestScopeNamed()
-//        {
-//            prepare
-//           var navigator = new NavigatorConfig()
-//               .UseMockFormProvider()
-//               .ToNavigator();
+            navigator.Forward(typeof(Named2Form));
 
-//            navigator.Register(ViewId.Named1Form, typeof(Named1Form));
-//            navigator.Register(ViewId.Named2Form, typeof(Named2Form));
+            var form2 = (Named2Form)navigator.CurrentView;
+            Assert.Same(form2.ImporttData, form1.ExportData);
+        }
 
-//            test
-//            navigator.Forward(ViewId.Named1Form);
+        public class Data1Form : MockForm
+        {
+        }
 
-//            var form1 = (Named1Form)navigator.CurrentView;
-//            Assert.NotNull(form1.ExportData);
+        public class Data2Form : MockForm
+        {
+            [Scope]
+            public ScopeData Data { get; set; }
+        }
 
-//            navigator.Forward(ViewId.Named2Form);
+        public class Data3Form : MockForm
+        {
+            [Scope]
+            public ScopeData Data { get; set; }
+        }
 
-//            var form2 = (Named2Form)navigator.CurrentView;
-//            Assert.Same(form2.ImporttData, form1.ExportData);
-//        }
+        public class Object1Form : MockForm
+        {
+            [Scope(typeof(ScopeObject))]
+            public IScopeObject Object { get; set; }
+        }
 
-//        public enum ViewId
-//        {
-//            Data1Form,
-//            Data2Form,
-//            Data3Form,
-//            Object1Form,
-//            Object2Form,
-//            Object3Form,
-//            Push1Form,
-//            Push2Form,
-//            Push3Form,
-//            Push4Form,
-//            Named1Form,
-//            Named2Form
-//        }
+        public class Object2Form : MockForm
+        {
+            [Scope]
+            public ScopeObject Object { get; set; }
+        }
 
-//        public class Data1Form : MockForm
-//        {
-//        }
+        public class Object3Form : MockForm
+        {
+        }
 
-//        public class Data2Form : MockForm
-//        {
-//            [Scope]
-//            public ScopeData Data { get; set; }
-//        }
+        public class Push1Form : MockForm
+        {
+        }
 
-//        public class Data3Form : MockForm
-//        {
-//            [Scope]
-//            public ScopeData Data { get; set; }
-//        }
+        public class Push2Form : MockForm
+        {
+            [Scope]
+            public ScopeData Data { get; set; }
+        }
 
-//        public class Object1Form : MockForm
-//        {
-//            [Scope(typeof(ScopeObject))]
-//            public IScopeObject Object { get; set; }
-//        }
+        public class Push3Form : MockForm
+        {
+        }
 
-//        public class Object2Form : MockForm
-//        {
-//            [Scope]
-//            public ScopeObject Object { get; set; }
-//        }
+        public class Push4Form : MockForm
+        {
+            [Scope]
+            public ScopeData Data { get; set; }
+        }
 
-//        public class Object3Form : MockForm
-//        {
-//        }
+        public class Named1Form : MockForm
+        {
+            [Scope("Data")]
+            public ScopeData ExportData { get; set; }
+        }
 
-//        public class Push1Form : MockForm
-//        {
-//        }
+        public class Named2Form : MockForm
+        {
+            [Scope("Data")]
+            public ScopeData ImporttData { get; set; }
+        }
 
-//        public class Push2Form : MockForm
-//        {
-//            [Scope]
-//            public ScopeData Data { get; set; }
-//        }
+        public sealed class ScopeData : IInitializable, IDisposable
+        {
+            public bool IsInitialized { get; private set; }
 
-//        public class Push3Form : MockForm
-//        {
-//        }
+            public bool IsDisposed { get; private set; }
 
-//        public class Push4Form : MockForm
-//        {
-//            [Scope]
-//            public ScopeData Data { get; set; }
-//        }
+            public void Initialize()
+            {
+                IsInitialized = true;
+            }
 
-//        public class Named1Form : MockForm
-//        {
-//            [Scope("Data")]
-//            public ScopeData ExportData { get; set; }
-//        }
+            public void Dispose()
+            {
+                IsDisposed = true;
+            }
+        }
 
-//        public class Named2Form : MockForm
-//        {
-//            [Scope("Data")]
-//            public ScopeData ImporttData { get; set; }
-//        }
+        public interface IScopeObject
+        {
+            int Value { get; set; }
+        }
 
-//        public sealed class ScopeData : IInitializable, IDisposable
-//        {
-//            public bool IsInitialized { get; private set; }
-
-//            public bool IsDisposed { get; private set; }
-
-//            public void Initialize()
-//            {
-//                IsInitialized = true;
-//            }
-
-//            public void Dispose()
-//            {
-//                IsDisposed = true;
-//            }
-//        }
-
-//        public interface IScopeObject
-//        {
-//            int Value { get; set; }
-//        }
-
-//        public class ScopeObject : IScopeObject
-//        {
-//            public int Value { get; set; }
-//        }
-//    }
-//}
+        public class ScopeObject : IScopeObject
+        {
+            public int Value { get; set; }
+        }
+    }
+}
