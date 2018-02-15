b0ad897f28ecd349fa34b01dcabf65f045cf4275
usausa <machi.pon@gmail.com>
2018-02-15 09:27:36 +0900

    Add uwp work project

---
+++ Example.FormsApp/Example.FormsApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Example.FormsApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadApplication(new FormsApp.App());
+        }
+    }
+}
