5b68a2229d69df54fc6a87d2544eacb86fa43d76
usausa <machi.pon@gmail.com>
2018-02-16 17:45:53 +0900

    Add view attached property

---
+++ Example.FormsApp/Example.FormsApp/Converters/ViewTitleConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Example.FormsApp.Converters
+{
+    using System;
+    using System.Globalization;
+
+    using Example.FormsApp.Views;
+
+    using Xamarin.Forms;
+
+    public sealed class ViewTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? ViewProperty.GetTitle((BindableObject)value) : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Example.FormsApp/Example.FormsApp/Views/ViewProperty.cs
@@ -0,0 +1,23 @@
+﻿namespace Example.FormsApp.Views
+{
+    using Xamarin.Forms;
+
+    public static class ViewProperty
+    {
+        public static readonly BindableProperty TitleProperty = BindableProperty.CreateAttached(
+            "Title",
+            typeof(string),
+            typeof(ViewProperty),
+            null);
+
+        public static string GetTitle(BindableObject view)
+        {
+            return (string)view.GetValue(TitleProperty);
+        }
+
+        public static void SetTitle(BindableObject view, string value)
+        {
+            view.SetValue(TitleProperty, value);
+        }
+    }
+}
