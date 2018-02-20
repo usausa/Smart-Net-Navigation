1d7defbe5517bcafa1a31c2484dfa0cbbd422fc9
usausa <machi.pon@gmail.com>
2018-02-20 17:41:43 +0900

    Update forms example

--- Example.FormsApp/Example.FormsApp/Views/Edit/EditDetailViewModel.cs
+++ Example.FormsApp/Example.FormsApp/Views/Edit/EditDetailViewModel.cs
@@ -1,6 +1,12 @@
 ﻿namespace Example.FormsApp.Views.Edit
 {
+    using Example.FormsApp.Services;
+
+    using Smart.Resolver.Attributes;
+
     public class EditDetailViewModel : AppViewModelBase
     {
+        [Inject]
+        public DataService DataService { get; set; }
     }
 }
--- Example.FormsApp/Example.FormsApp/Views/Edit/EditListViewModel.cs
+++ Example.FormsApp/Example.FormsApp/Views/Edit/EditListViewModel.cs
@@ -1,10 +1,16 @@
 ﻿namespace Example.FormsApp.Views.Edit
 {
+    using Example.FormsApp.Services;
+
     using Smart.Forms.Input;
     using Smart.Navigation;
+    using Smart.Resolver.Attributes;
 
     public class EditListViewModel : AppViewModelBase
     {
+        [Inject]
+        public DataService DataService { get; set; }
+
         public AsyncCommand<ViewId> Forward { get; }
 
         public EditListViewModel()
