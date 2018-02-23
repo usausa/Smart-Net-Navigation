6c237e5513985636a140f40b2e942f6e12c4047f
usausa <machi.pon@gmail.com>
2018-02-23 17:42:23 +0900

    Add view control interface

---
+++ Example.FormsApp/Example.FormsApp/Views/IViewControl.cs
@@ -0,0 +1,27 @@
+﻿namespace Example.FormsApp.Views
+{
+    public interface IViewControl
+    {
+        string Title { get; set; }
+
+        bool IsBusy { get; set; }
+
+        bool CanBack { get; set; }
+
+        bool Function1Enabled { get; set; }
+
+        string Function1Text { get; set; }
+
+        bool Function2Enabled { get; set; }
+
+        string Function2Text { get; set; }
+
+        bool Function3Enabled { get; set; }
+
+        string Function3Text { get; set; }
+
+        bool Function4Enabled { get; set; }
+
+        string Function4Text { get; set; }
+    }
+}
