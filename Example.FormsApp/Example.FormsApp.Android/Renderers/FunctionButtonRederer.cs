1b0bdbffe43a86fd51762a6970acccfeebf84954
usausa <machi.pon@gmail.com>
2018-02-27 17:55:46 +0900

    Update shell layout

---
+++ Example.FormsApp/Example.FormsApp.Android/Renderers/FunctionButtonRederer.cs
@@ -0,0 +1,27 @@
+﻿[assembly: Xamarin.Forms.ExportRenderer(typeof(Example.FormsApp.Controls.FunctionButton), typeof(Example.FormsApp.Droid.Renderers.FunctionButtonRederer))]
+
+namespace Example.FormsApp.Droid.Renderers
+{
+    using Android.Content;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Platform.Android;
+
+    public class FunctionButtonRederer : ButtonRenderer
+    {
+        public FunctionButtonRederer(Context context)
+            : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.Elevation = 0;
+            }
+        }
+    }
+}
