012392de879406e07cfc97ae5b2d7ae5111f7c7a
usausa <machi.pon@gmail.com>
2018-03-02 09:53:18 +0900

    Update edit list

--- Example.FormsApp/Example.FormsApp/Modules/Edit/EditListViewModel.cs
+++ Example.FormsApp/Example.FormsApp/Modules/Edit/EditListViewModel.cs
@@ -1,22 +1,59 @@
 ﻿namespace Example.FormsApp.Modules.Edit
 {
+    using System.Collections.ObjectModel;
+    using System.Threading.Tasks;
+
+    using Example.FormsApp.Models;
     using Example.FormsApp.Services;
 
+    using Smart.Collections.Generic;
     using Smart.Forms.Input;
     using Smart.Navigation;
     using Smart.Resolver.Attributes;
 
-    public class EditListViewModel : AppViewModelBase
+    public class EditListViewModel : AppViewModelBase, INavigationEventSupport, INotifySupportAsync<FunctionKeys>
     {
         [Inject]
         public DataService DataService { get; set; }
 
+        public ObservableCollection<DataEntity> Items { get; } = new ObservableCollection<DataEntity>();
+
         public AsyncCommand<ViewId> Forward { get; }
+
+        public AsyncCommand<DataEntity> Select { get; }
 
         public EditListViewModel(ApplicationState applicationState)
             : base(applicationState)
         {
             Forward = MakeAsyncCommand<ViewId>(x => Navigator.ForwardAsync(x));
+            Select = MakeAsyncCommand<DataEntity>(x =>
+                Navigator.ForwardAsync(ViewId.EditDetailUpdate, new NavigationParameter().SetValue(x)));
+        }
+
+        public void OnNavigatedFrom(INavigationContext context)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationContext context)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationContext context)
+        {
+            Items.AddRange(DataService.QueryDataList());
+        }
+
+        public Task NavigatorNotifyAsync(FunctionKeys parameter)
+        {
+            switch (parameter)
+            {
+                case FunctionKeys.Function1:
+                    return Navigator.ForwardAsync(ViewId.Menu);
+                case FunctionKeys.Function4:
+                    return Navigator.ForwardAsync(ViewId.EditDetailNew);
+                default:
+                    return Task.CompletedTask;
+            }
         }
     }
 }
