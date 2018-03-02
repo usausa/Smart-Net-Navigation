dd46274dbe0a9096fb4f9dc9618ab3d7bf141093
usausa <machi.pon@gmail.com>
2018-03-02 13:20:11 +0900

    Update windows example

---
+++ Example.WindowsApp/Views/ShellProperty.cs
@@ -0,0 +1,24 @@
+﻿namespace Example.WindowsApp.Views
+{
+    using System.Windows;
+
+    public static class ShellProperty
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.Maintainability", "SA1401:FieldsMustBePrivate", Justification = "Ignore")]
+        public static DependencyProperty TitleProperty = DependencyProperty.RegisterAttached(
+            "Title",
+            typeof(string),
+            typeof(ShellProperty),
+            new PropertyMetadata(string.Empty));
+
+        public static string GetTitle(DependencyObject obj)
+        {
+            return (string)obj.GetValue(TitleProperty);
+        }
+
+        public static void SetTitle(DependencyObject obj, string value)
+        {
+            obj.SetValue(TitleProperty, value);
+        }
+    }
+}
