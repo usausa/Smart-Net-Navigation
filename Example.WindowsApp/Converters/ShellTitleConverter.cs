9eb90199c3bc477ca6ee740a623cc7b63e87099f
usausa <machi.pon@gmail.com>
2018-07-02 16:45:29 +0900

    Update windows example

---
+++ Example.WindowsApp/Converters/ShellTitleConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Example.WindowsApp.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    using Example.WindowsApp.Views;
+
+    [ValueConversion(typeof(object), typeof(string))]
+    public sealed class ShellTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? string.Empty : ShellProperty.GetTitle((DependencyObject)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
--- Example.WindowsApp/MainWindowViewModel.cs
+++ Example.WindowsApp/MainWindowViewModel.cs
@@ -3,12 +3,16 @@
     using System;
     using System.Reactive.Linq;
 
+    using Smart.ComponentModel;
+
     using Smart.Navigation;
     using Smart.Windows.ViewModels;
 
     public class MainWindowViewModel : ViewModelBase
     {
         public INavigator Navigator { get; }
+
+        public NotificationValue<object> CurrentView { get; } = new NotificationValue<object>();
 
         public MainWindowViewModel(INavigator navigator)
         {
@@ -17,6 +21,12 @@
             Disposables.Add(Observable
                 .FromEvent<EventHandler<EventArgs>, EventArgs>(h => (s, e) => h(e), h => navigator.ExecutingChanged += h, h => navigator.ExecutingChanged -= h)
                 .Subscribe(e => BusyState.IsBusy = navigator.Executing));
+            Disposables.Add(Observable
+                .FromEvent<EventHandler<NavigationEventArgs>, NavigationEventArgs>(h => (s, e) => h(e), h => navigator.Navigated += h, h => navigator.Navigated -= h)
+                .Subscribe(e => CurrentView.Value = e.ToView));
+            Disposables.Add(Observable
+                .FromEvent<EventHandler<EventArgs>, EventArgs>(h => (s, e) => h(e), h => navigator.Exited += h, h => navigator.Exited -= h)
+                .Subscribe(e => CurrentView.Value = null));
         }
     }
 }
