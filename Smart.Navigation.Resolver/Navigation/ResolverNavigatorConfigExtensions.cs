aee4cae1fa5034bcaf02ebd6d07955d179318627
usausa <machi.pon@gmail.com>
2022-06-23 17:10:02 +0900

    Rename method

--- Smart.Navigation.Resolver/Navigation/ResolverNavigatorConfigExtensions.cs
+++ Smart.Navigation.Resolver/Navigation/ResolverNavigatorConfigExtensions.cs
@@ -4,7 +4,7 @@ using Smart.Navigation.Plugins.Resolver;
 
 public static class ResolverNavigatorConfigExtensions
 {
-    public static NavigatorConfig UseResolverPlugin(this NavigatorConfig config)
+    public static NavigatorConfig AddResolverPlugin(this NavigatorConfig config)
     {
         config.AddPlugin<ResolverPlugin>();
         return config;
