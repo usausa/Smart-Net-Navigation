a4d231c117c8e12ee7e37dd078198ef30c88afb5
usausa <machi.pon@gmail.com>
2025-12-06 13:29:12 +0900

    Update generator

--- Smart.Navigation.Generator/Navigation/Generator/Diagnostics.cs
+++ Smart.Navigation.Generator/Navigation/Generator/Diagnostics.cs
@@ -4,7 +4,7 @@ using Microsoft.CodeAnalysis;
 
 internal static class Diagnostics
 {
-    public static DiagnosticDescriptor InvalidMethodDefinition => new(
+    public static DiagnosticDescriptor InvalidMethodDefinition { get; } = new(
         id: "SNV0001",
         title: "Invalid method definition",
         messageFormat: "Method must be partial extension. method=[{0}]",
@@ -12,7 +12,7 @@ internal static class Diagnostics
         defaultSeverity: DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
 
-    public static DiagnosticDescriptor InvalidMethodParameter => new(
+    public static DiagnosticDescriptor InvalidMethodParameter { get; } = new(
         id: "SNV0002",
         title: "Invalid method parameter",
         messageFormat: "Parameter count must be nothing. method=[{0}]",
@@ -20,7 +20,7 @@ internal static class Diagnostics
         defaultSeverity: DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
 
-    public static DiagnosticDescriptor InvalidMethodReturnType => new(
+    public static DiagnosticDescriptor InvalidMethodReturnType { get; } = new(
         id: "SNV0003",
         title: "Invalid method return type",
         messageFormat: "Return type must be IEnumerable<KeyValuePair<ViewId, Type>>. method=[{0}]",
