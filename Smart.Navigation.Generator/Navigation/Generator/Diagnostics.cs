23c46e3b9a151d0adf1c8383ea6b57c09a8c1faf
usausa <machi.pon@gmail.com>
2026-08-29 09:32:56 +0900

    Update

--- Smart.Navigation.Generator/Navigation/Generator/Diagnostics.cs
+++ Smart.Navigation.Generator/Navigation/Generator/Diagnostics.cs
@@ -7,7 +7,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidMethodDefinition { get; } = new(
         id: "SNV0001",
         title: "Invalid method definition",
-        messageFormat: "Method must be partial extension. method=[{0}]",
+        messageFormat: "[ViewSource] method must be partial extension. method=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
@@ -15,7 +15,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidMethodParameter { get; } = new(
         id: "SNV0002",
         title: "Invalid method parameter",
-        messageFormat: "Method must not have parameters. method=[{0}]",
+        messageFormat: "[ViewSource] method must not have parameters. method=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
@@ -23,7 +23,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidMethodReturnType { get; } = new(
         id: "SNV0003",
         title: "Invalid method return type",
-        messageFormat: "Return type must be IEnumerable<KeyValuePair<ViewId, Type>>. method=[{0}]",
+        messageFormat: "[ViewSource] return type must be IEnumerable<KeyValuePair<ViewId, Type>>. method=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
