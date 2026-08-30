083e4d76a7232f071ab2453b5ac81606084a6889
usausa <machi.pon@gmail.com>
2026-08-30 15:54:01 +0900

    Update

--- Example.WindowsApp/GlobalSuppressions.cs
+++ Example.WindowsApp/GlobalSuppressions.cs
@@ -1,2 +1,1 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Ignore")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0130", Justification = "False positive")]
--- Smart.Navigation.Tests/GlobalSuppressions.cs
+++ Smart.Navigation.Tests/GlobalSuppressions.cs
@@ -3,4 +3,4 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1052:StaticHolderTypesShouldBeSealed", Justification = "Ignore")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Ignore")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:DisposeObjectsBeforeLosingScope", Justification = "Ignore")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:DoNotDirectlyAwaitATask", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "Ignore")]
